b53d6038807792f3bf5364f4058fdd39fa33d3e5
Lejia Chen <monsoon.cl@gmail.com>
2020-04-01 14:34:25 +0800

    Add fully-qualified image references for auto updates

--- tlh.sh
+++ tlh.sh
@@ -14,7 +14,7 @@ cp -r www current/
 cd current
 sudo podman pod create --name=tlh -p 80 -p 443
 sudo podman create --name=caddy --pod=tlh -v ./Caddyfile:/etc/caddy/Caddyfile:Z -v ./www:/var/www:Z -v ./caddy_data_directory:/root/.local/share/caddy:Z --label io.containers.autoupdate=image caddy/caddy:alpine
-sudo podman create --name=v2ray --pod=tlh -v ./v2ray_service.json:/etc/v2ray/config.json:Z --label io.containers.autoupdate=image v2fly/v2fly-core
+sudo podman create --name=v2ray --pod=tlh -v ./v2ray_service.json:/etc/v2ray/config.json:Z --label io.containers.autoupdate=image v2fly/v2fly-core:latest
 
 sudo podman generate systemd --files --name tlh
 sudo cp {pod-tlh,container-caddy,container-v2ray}.service /etc/systemd/system/
