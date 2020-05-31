f3b8fcddd3ec5002f93ce3cf987e09421e12034d
Lejia Chen <monsoon.cl@gmail.com>
2020-05-31 19:48:15 +0800

    Remove the docker images when uninstalling

--- tlh_uninstall.sh
+++ tlh_uninstall.sh
@@ -3,3 +3,7 @@
 sudo systemctl disable --now pod-tlh
 sudo rm /etc/systemd/system/{pod-tlh,container-star-link,container-caddy,container-v2ray}.service
 sudo podman pod rm tlh
+
+for repo_name in caddy:latest aasterism/star-link:latest v2fly/v2fly-core:latest; do
+  sudo podman rmi $(sudo podman images -q "$repo_name")
+done
