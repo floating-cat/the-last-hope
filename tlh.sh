88a4e24215ae284227d5fd7a68c628113145d116
Lejia Chen <monsoon.cl@gmail.com>
2020-05-31 19:47:06 +0800

    Update caddy image

--- tlh.sh
+++ tlh.sh
@@ -12,7 +12,7 @@ done
 cp -r www current/
 cd current
 sudo podman pod create --name=tlh -p 80 -p 443
-sudo podman create --name=caddy --pod=tlh -v ./Caddyfile:/etc/caddy/Caddyfile:Z -v ./www:/var/www:Z -v ./caddy_data_directory:/root/.local/share/caddy:Z caddy/caddy:alpine
+sudo podman create --name=caddy --pod=tlh -v ./Caddyfile:/etc/caddy/Caddyfile:Z -v ./www:/var/www:Z -v ./caddy_data_directory:/root/.local/share/caddy:Z caddy:latest
 sudo podman create --name=v2ray --pod=tlh -v ./v2ray_service.json:/etc/v2ray/config.json:Z v2fly/v2fly-core:latest
 
 sudo podman generate systemd --files --name tlh
