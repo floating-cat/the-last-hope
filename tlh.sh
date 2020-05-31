e244dfb5c7133635b408e094a650b58072d474d6
floating-cat <monsoon.cl@gmail.com>
2020-05-31 19:47:49 +0800

    Support star-link

--- tlh.sh
+++ tlh.sh
@@ -1,20 +1,35 @@
 #!/bin/sh
 
-read -p "Please enter your domain: " domain
+if [ ! -d current ]; then
+  read -p "Please enter your domain: " domain
 
-wspath=$(uuidgen)
-v2ray_id=$(uuidgen)
-mkdir -p current/caddy_data_directory
-for file_name in Caddyfile v2ray_service.json v2ray_client_template.json; do
-  sed "s/example.com/$domain/g; s/wspath_placeholder/$wspath/g; s/v2ray_id_placeholder/$v2ray_id/g" "$file_name" >"current/$file_name"
-done
+  star_link_password=$(openssl rand -hex 16)
+  # https://superuser.com/a/416630
+  star_link_wspath=$(echo "$star_link_password" | xxd -r -p | base64)
+
+  v2ray_wspath=$(uuidgen)
+  v2ray_id=$(uuidgen)
+  mkdir -p current/caddy_data_directory
+  for file_name in Caddyfile server.conf client.conf v2ray_service.json v2ray_client_template.json; do
+    sed "s/domain_placeholder/$domain/g; \
+    s/star_link_wspath_placeholder/$star_link_wspath/g; \
+    s/star_link_password_placeholder/$star_link_password/g; \
+    s/v2ray_wspath_placeholder/$v2ray_wspath/g; \
+    s/v2ray_id_placeholder/$v2ray_id/g" \
+      "$file_name" >"current/$file_name"
+  done
+
+else
+  echo "\`current\` directory exists. Skip to create relevant configuration files."
+fi
 
 cp -r www current/
 cd current
 sudo podman pod create --name=tlh -p 80 -p 443
 sudo podman create --name=caddy --pod=tlh -v ./Caddyfile:/etc/caddy/Caddyfile:Z -v ./www:/var/www:Z -v ./caddy_data_directory:/root/.local/share/caddy:Z caddy:latest
+sudo podman create --name=star-link --pod=tlh -v ./server.conf:/etc/star-link/server.conf:Z aasterism/star-link:latest
 sudo podman create --name=v2ray --pod=tlh -v ./v2ray_service.json:/etc/v2ray/config.json:Z v2fly/v2fly-core:latest
 
 sudo podman generate systemd --files --name tlh
-sudo cp {pod-tlh,container-caddy,container-v2ray}.service /etc/systemd/system/
+sudo cp {pod-tlh,container-star-link,container-caddy,container-v2ray}.service /etc/systemd/system/
 sudo systemctl enable --now pod-tlh
--- tlh_uninstall.sh
+++ tlh_uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sudo systemctl disable --now pod-tlh
-sudo rm /etc/systemd/system/{pod-tlh,container-caddy,container-v2ray}.service
+sudo rm /etc/systemd/system/{pod-tlh,container-star-link,container-caddy,container-v2ray}.service
 sudo podman pod rm tlh
