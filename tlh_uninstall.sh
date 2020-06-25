b26d4e88b812bab52cf5560772cbbb56b93df6fa
floating-cat <monsoon.cl@gmail.com>
2020-06-25 16:14:44 +0800

    Improve shell scripts by using ShellCheck

--- tlh_uninstall.sh
+++ tlh_uninstall.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo systemctl disable --now pod-tlh
 sudo rm /etc/systemd/system/{pod-tlh,container-star-link,container-caddy,container-v2ray}.service
 sudo podman pod rm tlh
 
-read -p "Do you want to remove the related container images? [y/N] " yN
+read -r -p "Do you want to remove the related container images? [y/N] " yN
 yN=${yN,,} # to lowercase
 
 if [[ "$yN" =~ ^(y|yes)$ ]]; then
   for repo_name in caddy:latest aasterism/star-link:latest v2fly/v2fly-core:latest; do
-    sudo podman rmi $(sudo podman images -q "$repo_name")
+    sudo podman rmi "$(sudo podman images -q "$repo_name")"
   done
 fi
